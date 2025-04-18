61f282769b257eab7ad555452cf89104e363dbdc
not-the <87151784+not-the@users.noreply.github.com>
2025-04-18 18:31:10 -0400

    /support page w/ redirect

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    // Redirect URL
+    const redirectURL = "https://ko-fi.com/A0A41DPFA8";
+</script>
+
+
+<!-- HEAD -->
+<svelte:head>
+    <title>Support notkal.com</title>
+
+    <!-- Redirect -->
+    <meta http-equiv="refresh" content={`0; url=${redirectURL}`} />
+</svelte:head>
+
+
+<!-- HTML -->
+<a href={redirectURL} rel="noreferrer">
+    Click here if you aren't redirected
+</a>
+
