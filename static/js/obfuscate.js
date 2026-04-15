78110af3767d64a1088a69ac3c2730090998269c
not-the <87151784+not-the@users.noreply.github.com>
2026-04-15 18:10:29 -0400

    obfuscate.js support for multiple elements

--- static/js/obfuscate.js
+++ static/js/obfuscate.js
@@ -1,5 +1,5 @@
 // Obfuscation
-document.querySelector('[data-obfuscated]').addEventListener("click", e => {
+document.querySelectorAll('[data-obfuscated]').forEach(obfElement => obfElement.addEventListener("click", e => {
     let value = '&#x63;&#x6F;&#x6E;&#x74;&#x61;&#x63;&#x74;&#x40;&#x6E;&#x6F;&#x74;&#x6B;&#x61;&#x6C;&#x2E;&#x63;&#x6F;&#x6D;';
     let [a, b, c, d] = ['al.com', '@notk', 'con', 'tact'];
     const button = e.currentTarget;
@@ -7,4 +7,4 @@ document.querySelector('[data-obfuscated]').addEventListener("click", e => {
     [element.href, element.target, element.rel] = [`mailto:${c}${d}${b}${a}`, "_blank", "noreferrer"];
     element.innerHTML = `${value}`;
     button.replaceWith(element);
-})
+}))
